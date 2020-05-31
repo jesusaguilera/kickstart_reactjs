8ca3fd53c1b90a988674d4e2325a5f9e475a3e9e
jesusaguilera <jesusaguilera09@gmail.com>
2020-05-31 17:01:41 +0200

    Updates

---
+++ precache-manifest.10290c7e9df6269e53bb51e706dfd960.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4a60b35e32a99c9e0edded62611b043",
+    "url": "/personal_site/index.html"
+  },
+  {
+    "revision": "173a8a6620af8f1a43e5",
+    "url": "/personal_site/static/css/main.c32b6f20.chunk.css"
+  },
+  {
+    "revision": "2684f5c6f742f9aa160b",
+    "url": "/personal_site/static/js/2.497484bd.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/personal_site/static/js/2.497484bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "173a8a6620af8f1a43e5",
+    "url": "/personal_site/static/js/main.cb0c2ff5.chunk.js"
+  },
+  {
+    "revision": "303eaf744ed4b12c97d3",
+    "url": "/personal_site/static/js/runtime-main.6859d38d.js"
+  },
+  {
+    "revision": "c450749217dc745e41039f2cb53dfdb2",
+    "url": "/personal_site/static/media/Averta-Bold.c4507492.ttf"
+  },
+  {
+    "revision": "712ee2bd43f2a1dc81b398582957d5a2",
+    "url": "/personal_site/static/media/Averta-Regular.712ee2bd.ttf"
+  },
+  {
+    "revision": "42d202b79a6caeff70353b3b05624e23",
+    "url": "/personal_site/static/media/Averta-Semibold.42d202b7.ttf"
+  }
+]);
