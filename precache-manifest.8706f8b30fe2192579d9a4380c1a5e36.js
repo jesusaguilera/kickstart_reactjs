09bcbdf4cfc46cb4b1768ed32c1679241a808f1b
jesusaguilera <jesusaguilera09@gmail.com>
2020-08-17 15:29:26 +0200

    Updates

---
+++ precache-manifest.8706f8b30fe2192579d9a4380c1a5e36.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d08d1e3823d8eb45b16217948858b0",
+    "url": "/kickstart_reactjs/index.html"
+  },
+  {
+    "revision": "6df5263b9bcbe21b7304",
+    "url": "/kickstart_reactjs/static/css/main.40720bb9.chunk.css"
+  },
+  {
+    "revision": "0788ecdafd5dd38fae98",
+    "url": "/kickstart_reactjs/static/js/2.17a3b4bd.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/kickstart_reactjs/static/js/2.17a3b4bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6df5263b9bcbe21b7304",
+    "url": "/kickstart_reactjs/static/js/main.1d3f171a.chunk.js"
+  },
+  {
+    "revision": "ea8da506be09a05b976d",
+    "url": "/kickstart_reactjs/static/js/runtime-main.7e52bfe5.js"
+  },
+  {
+    "revision": "c450749217dc745e41039f2cb53dfdb2",
+    "url": "/kickstart_reactjs/static/media/Averta-Bold.c4507492.ttf"
+  },
+  {
+    "revision": "712ee2bd43f2a1dc81b398582957d5a2",
+    "url": "/kickstart_reactjs/static/media/Averta-Regular.712ee2bd.ttf"
+  },
+  {
+    "revision": "42d202b79a6caeff70353b3b05624e23",
+    "url": "/kickstart_reactjs/static/media/Averta-Semibold.42d202b7.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal_site/precache-manifest.016e05be6c7c3012a0fd3fb899945daa.js"
+  "/kickstart_reactjs/precache-manifest.8706f8b30fe2192579d9a4380c1a5e36.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal_site/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kickstart_reactjs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
